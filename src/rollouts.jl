f29a3ab6b5a8d9fd456cca25d3e615cb81958dc4
Andy Goldschmidt <andy.goldschmidt@jhuapl.edu>
2025-12-16 11:11:26 -0500

    MatrixOperator integrators

--- src/rollouts.jl
+++ src/rollouts.jl
@@ -11,7 +11,9 @@ Provides a domain specific language for quantum system rollouts, with functions
 """
 
 export KetODEProblem
+export KetOperatorODEProblem
 export UnitaryODEProblem
+export UnitaryOperatorODEProblem
 export DensityODEProblem
 
 using LinearAlgebra
@@ -86,37 +88,25 @@ function PiccoloRolloutSystem(
     return PiccoloRolloutSystem(state_index, control_index, timestep_name, defaults)
 end
 
-
-function KetODEProblem(
-    sys::AbstractQuantumSystem, u::F, ψ0::Vector{ComplexF64}, T::Real; 
-    state_name::Symbol=:ψ,
-    control_name::Symbol=:u,
-    kwargs...
-) where F
-	rhs!(dx, x, p, t) = mul!(dx, sys.H(u(t), t), x, -im, 0.0)
-    sii_sys = PiccoloRolloutSystem(state_name => sys.levels, control_name => sys.n_drives)
-	return ODEProblem(ODEFunction(rhs!; sys = sii_sys), ψ0, (0, T); kwargs...)
+function _construct_operator(sys::AbstractQuantumSystem, u::F) where F
+    A0 = zeros(ComplexF64, sys.levels, sys.levels)
+    function update!(A, x, p, t)
+        Ht = collect(sys.H(u(t), t))
+        @. A = -im * Ht
+        return nothing
+    end
+    return SciMLOperators.MatrixOperator(A0; update_func! = update!)
 end
 
-function UnitaryODEProblem(
-    sys::AbstractQuantumSystem, u::F, T::Real; 
-    state_name::Symbol=:U, 
-    control_name::Symbol=:u,
-    kwargs...
-) where F
-	rhs!(dx, x, p, t) = mul!(dx, sys.H(u(t), t), x, -im, 0.0)
-    U0 = Matrix{ComplexF64}(I, sys.levels, sys.levels)
-    sii_sys = PiccoloRolloutSystem(
-        state_name => (sys.levels, sys.levels), control_name => sys.n_drives)
-	return ODEProblem(ODEFunction(rhs!; sys = sii_sys), U0, (0, T); kwargs...)
+function _construct_rhs(sys::AbstractQuantumSystem, u::F) where F 
+    function rhs!(dx, x, p, t)
+        mul!(dx, sys.H(u(t), t), x, -im, 0.0)
+        return nothing
+    end
+    return rhs!
 end
 
-function DensityODEProblem(
-    sys::OpenQuantumSystem, u::F, ρ0::Matrix{ComplexF64}, T::Real; 
-    state_name::Symbol=:ρ,
-    control_name::Symbol=:u,
-    kwargs...
-) where F
+function _construct_rhs(sys::OpenQuantumSystem, u::F) where F
     Ls = sys.dissipation_operators
     Ks = map(L -> adjoint(L) * L, Ls)  # precompute L†L once
     tmp = similar(ρ0)  # buffer
@@ -140,11 +130,96 @@ function DensityODEProblem(
         return nothing
     end
 
-    sii_sys = PiccoloRolloutSystem(
-        state_name => (sys.levels, sys.levels), control_name => sys.n_drives)
+    return rhs!
+end
+
+# ------------------------------------------
+# Standard, sparse ODE integrators
+# ------------------------------------------
+
+function KetODEProblem(
+    sys::AbstractQuantumSystem, u::F, ψ0::Vector{ComplexF64}, T::Real; 
+    state_name::Symbol=:ψ,
+    control_name::Symbol=:u,
+    kwargs...
+) where F
+	rhs! = _construct_rhs(sys, u)
+    sii_sys = PiccoloRolloutSystem(state_name => sys.levels, control_name => sys.n_drives)
+	return ODEProblem(ODEFunction(rhs!; sys = sii_sys), ψ0, (0, T); kwargs...)
+end
+
+function UnitaryODEProblem(
+    sys::AbstractQuantumSystem, u::F, T::Real; 
+    state_name::Symbol=:U, 
+    control_name::Symbol=:u,
+    kwargs...
+) where F
+    n = sys.levels
+	rhs! = _construct_rhs(sys, u)
+    U0 = Matrix{ComplexF64}(I, n, n)
+    sii_sys = PiccoloRolloutSystem(state_name => (n, n), control_name => sys.n_drives)
+	return ODEProblem(ODEFunction(rhs!; sys = sii_sys), U0, (0, T); kwargs...)
+end
+
+function DensityODEProblem(
+    sys::OpenQuantumSystem, u::F, ρ0::Matrix{ComplexF64}, T::Real; 
+    state_name::Symbol=:ρ,
+    control_name::Symbol=:u,
+    kwargs...
+) where F
+    n = sys.levels
+	rhs! = _construct_rhs(sys, u)
+    sii_sys = PiccoloRolloutSystem(state_name => (n, n), control_name => sys.n_drives)
 	return ODEProblem(ODEFunction(rhs!; sys = sii_sys), ρ0, (0, T); kwargs...)
 end
 
+# ------------------------------------------
+# Lie Group ODE solvers (e.g., Magnus)
+# ------------------------------------------
+# TODO: Operator integrator for Density
+
+function KetOperatorODEProblem(
+    sys::AbstractQuantumSystem, 
+    u::F, 
+    ψ0::Vector{ComplexF64}, 
+    times::AbstractVector{<:Real}; 
+    state_name::Symbol=:ψ,
+    control_name::Symbol=:u,
+    kwargs...
+) where F
+    op! = _construct_operator(sys, u)
+    sii_sys = PiccoloRolloutSystem(state_name => sys.levels, control_name => sys.n_drives)
+	return ODEProblem(
+        ODEFunction(op!; sys = sii_sys), 
+        ψ0, 
+        (0, times[end]);
+        tstops=times, 
+        saveat=times,
+        kwargs...
+     )
+end
+
+function UnitaryOperatorODEProblem(
+    sys::AbstractQuantumSystem, 
+    u::F, 
+    times::AbstractVector{<:Real}; 
+    state_name::Symbol=:U, 
+    control_name::Symbol=:u,
+    kwargs...
+) where F
+    n = sys.levels
+    op! = _construct_operator(sys, u)
+    U0 = Matrix{ComplexF64}(I, n, n)
+    sii_sys = PiccoloRolloutSystem(state_name => (n, n), control_name => sys.n_drives)
+	return ODEProblem(
+        ODEFunction(op!; sys = sii_sys), 
+        U0,
+        (0, times[end]);
+        tstops=times, 
+        saveat=times,
+        kwargs...
+    )
+end
 
 # ------------------------------------------------------------ #
 # Minimal interface 
